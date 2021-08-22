4f4ff25bddfef27357a78b5af9cdacb9a117e349
CutrelyAlex <88236310+CutrelyAlex@users.noreply.github.com>
2021-08-23 03:40:52 +0800

    Add files via upload

---
+++ day18_2.c
@@ -0,0 +1,19 @@
+/*预处理*/
+#include<stdio.h>
+//预定义符号
+//进行编译的源文件 __FILE__
+//文件当前行号 __LINE__
+//文件被编译的日期 __DATE__
+//文件被编译的时间 __TIME__
+
+//int main ()
+//{
+//	printf ( "%s\n", __FILE__ );
+//	printf ( "%d\n", __LINE__);
+//	printf ( "%s\n", __DATE__ );
+//	printf ( "%s\n", __TIME__ );
+//	return 0;
+//}
+
+//define
+
